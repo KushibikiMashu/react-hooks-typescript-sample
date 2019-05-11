f8e1ad8af497124b83489d173b256020fb112b7d
KushibikiMashu <kushibiki3413@gmail.com>
2019-05-12 01:49:59 +0900

    データ取得の状態でコンポーネントを出しわけ

--- src/hooks/Todo.tsx
+++ src/hooks/Todo.tsx
@@ -1,47 +1,103 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Dispatch, SetStateAction } from "react";
 import axios from "axios";
 
 // custom hook
 
 interface Todo {
-  userId: number;
-  title: string;
-  completed: boolean;
+  userId?: number;
+  title?: string;
+  completed?: boolean;
 }
 
-function useFetchTodo(todoId: number): null | Todo {
-  const [article, setTodo] = useState(null);
+enum Status {
+  Loading = "loading",
+  Success = "success",
+  Error = "error"
+}
+
+function useFetchTodo(
+  todoId: number,
+  setStatus: Dispatch<SetStateAction<Status>>
+): null | Todo {
+  const [todo, setTodo] = useState(null);
 
   useEffect((): void => {
     async function fetch(): Promise<any> {
-      const res = await axios.get(
-        `https://jsonplaceholder.typicode.com/todos/${todoId}`
-      );
-      setTodo(res.data);
+      try {
+        const res = await axios.get(
+          `https://jsonplaceholder.typicode.com/todos/${todoId}`
+        );
+        setTodo(res.data);
+        setStatus(Status.Success);
+      } catch (e) {
+        console.error(e.messages);
+        setStatus(Status.Error);
+      }
     }
 
     fetch();
   }, [todoId]);
 
-  return article;
+  return todo;
 }
 
-const Todo: React.FC = () => {
-  const [todoId, setTodoId] = useState(1);
-  const article = useFetchTodo(todoId);
+const Loading: React.FC = () => <>'Loading'</>;
 
-  if (article === null) {
-    return <>'Loading...'</>;
+const TodoItem: React.FC<Todo> = ({ userId, title, completed }) => {
+  return (
+    <>
+      <p>User ID : {userId}</p>
+      <p>Title : {title}</p>
+      <p>Completed : {completed ? "true" : "false"}</p>
+    </>
+  );
+};
+
+/**
+ * Todo
+ */
+// const todoItem = (status: Status, todo: Todo) => {
+//   switch (status) {
+//     case Status.Loading:
+//       return <Loading/>;
+//     case Status.Success:
+//       return <TodoItem {...todo}/>;
+//     case Status.Error:
+//       return <>error</>;
+//     default:
+//       break;
+//   }
+// };
+
+const Todo: React.FC = () => {
+  const [todoId, setTodoId] = useState<number>(1);
+  const [status, setStatus] = useState<Status>(Status.Loading);
+  const todo = useFetchTodo(todoId, setStatus);
+
+  if (todo === null) {
+    return <Loading />;
   }
 
   return (
     <>
-      <button onClick={() => setTodoId(todoId + 1)}>+</button>
-      <button onClick={() => setTodoId(1)}>reset</button>
-      <p>User ID : {article.userId}</p>
+      <button
+        onClick={() => {
+          setTodoId(todoId + 1);
+          setStatus(Status.Loading);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setTodoId(1);
+          setStatus(Status.Loading);
+        }}
+      >
+        reset
+      </button>
       <p>Todo ID: {todoId}</p>
-      <p>Title : {article.title}</p>
-      <p>Completed : {article.completed ? "true" : "false"}</p>
+      {status === Status.Success ? <TodoItem {...todo} /> : Status.Loading}
     </>
   );
 };
