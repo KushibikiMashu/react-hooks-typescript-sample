43130982b9af7860ac6132f14eb3f391390907c5
KushibikiMashu <kushibiki3413@gmail.com>
2019-05-15 23:08:45 +0900

    React Hooksのサンプルを１ページにまとめる

---
+++ src/AppRouter.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Counter from "./hooks/Counter";
+import Todo from "./hooks/Todo";
+import CounterWithContext from "./hooks/CounterWithContext";
+import CounterWithReducer from "./hooks/CounterWithReducer";
+import FibonacciCounter from "./hooks/FibonacciCounter";
+import TextInput from "./hooks/TextInput";
+import TodoA from "./hooks/TodoA";
+
+const AppRouter: React.FC = () => {
+  return (
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          useState: <Link to="/usestate">counter</Link>
+        </li>
+        <li>
+          useEffect: <Link to="/useeffect">data fetch</Link>
+        </li>
+        <li>
+          useContext: <Link to="/usecontext">counter</Link>
+        </li>
+        <li>
+          useReducer: <Link to="/usereducer">counter</Link>
+        </li>
+        <li>
+          useMemo: <Link to="/usememo">Fibonacci Counter</Link>
+        </li>
+        <li>
+          useRef: <Link to="/useref">text input</Link>
+        </li>
+        <li>
+          Custom Hook: <Link to="/customhook">data fetch</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <div>
+        <Route exact path="/" component={() => <></>} />
+        <Route path="/usestate" component={Counter} />
+        <Route path="/useeffect" component={Todo} />
+        <Route path="/usecontext" component={CounterWithContext} />
+        <Route path="/usereducer" component={CounterWithReducer} />
+        <Route path="/usememo" component={() => <FibonacciCounter n={36} />} />
+        <Route path="/useref" component={TextInput} />
+        <Route path="/customhook" component={TodoA} />
+      </div>
+    </Router>
+  );
+};
+
+export default AppRouter;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import AppRouter from "./AppRouter";
 import * as serviceWorker from "./serviceWorker";
 
-import CounterWithContext from "./hooks/CounterWithContext";
-
-ReactDOM.render(<CounterWithContext />, document.getElementById("root"));
+ReactDOM.render(<AppRouter />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
